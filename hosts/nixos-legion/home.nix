6fea8bf37e8923f67c61cf08415c3a011ec36e5a
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-07-12 18:28:25 -0400

    added home.nix

---
+++ hosts/nixos-legion/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+  ../../modules/home-manager/shared.nix
+  ];
+  
+  # GNOME config: Use 'dconf watch /'
+  dconf.settings = {
+    "org/gnome/shell" = {
+      last-selected-power-profile = "performance";
+    };
+
+    # Dash to Panel
+    "org/gnome/shell/extensions/dash-to-panel" = {
+#      panel-sizes = {"0":32,"1":32,"2":32};
+#      panel-element-positions = {"0":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}],"1":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}],"2":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]};
+    };
+
+    
+    # Tiling Assistant
+    "org/gnome/shell/extensions/tiling-assistant" = {
+      window-gap = 8;
+      single-screen-gap = 8;
+      maximize-with-gap = true;
+    };
+  };
+  
+
+  # Home Enviorment Packages
+  home.packages = with pkgs; [
+
+  ];
+
+  # Home Manager starting version, do not touch
+  home.stateVersion = "23.11";
+}
