f6eeafea986127c413573dee61157cfedfa81550
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-07-25 10:56:46 -0400

    temp mv file

---
+++ hosts/homelab/configuration.nix
@@ -0,0 +1,106 @@
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  inputs,
+  lib,
+  vars,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.nix
+    ../../modules/nixos/shared.nix
+    ../../modules/nixos/profiles/server.nix
+  ];
+
+  # System installed pkgs
+  environment.systemPackages =
+    (with pkgs; [
+      # STABLE installed packages
+      zfs
+    ])
+    ++ (with pkgs-unstable; [
+      # UNSTABLE installed packages
+    ]);
+
+  # lock down users
+  users.mutableUsers = false;
+  #users.users.root.hashedPassword = "!";
+
+  # Define your hostname.
+  networking.hostName = "homelab";
+
+  #security
+  # lock down nix
+  nix.settings.allowed-users = ["root"];
+  # disable sudo
+  security.sudo.enable = false;
+
+  # ssh server
+  users.users.root.openssh.authorizedKeys.keys = vars.publicSshKeys;
+  services.openssh = {
+    enable = true;
+    allowSFTP = true;
+    settings.KbdInteractiveAuthentication = false;
+    extraConfig = ''
+      passwordAuthentication = no
+      PermitRootLogin = prohibit-password
+      AllowTcpForwarding yes
+      X11Forwarding no
+      AllowAgentForwarding no
+      AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
+      PermitTunnel no
+    '';
+  };
+
+  # zfs support
+  boot.supportedFilesystems = ["zfs"];
+  ##   environment.systemPackages = with pkgs; [zfs];
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
+  };
+  networking.hostId = "e0019fd8";
+
+  # impermanance
+  fileSystems."/nix/state".neededForBoot = true;
+  fileSystems."/nix".neededForBoot = true;
+  boot.initrd = {
+    systemd = {
+      enable = true;
+      services.rollback = {
+        description = "Rollback root filesystem to a pristine state on boot";
+        wantedBy = ["initrd.target"];
+        after = ["zfs-import-zroot.service"];
+        before = ["sysroot.mount"];
+        path = with pkgs; [zfs];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          zfs rollback -r zroot/local/root@blank && echo "  >> >> ROLLBACK COMPLETE << <<"
+        '';
+      };
+    };
+  };
+
+  # persistence
+  environment.persistence."/nix/state" = {
+    # https://github.com/nix-community/impermanence?tab=readme-ov-file#module-usage
+    enable = true;
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/etc/nixos"
+      "/var/lib/systemd/timers" # for systemd persistant timers during off time
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+}
