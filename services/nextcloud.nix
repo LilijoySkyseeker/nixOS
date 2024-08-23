fa52b20b1fe1995f5efa3be2262ef5788abd92c6
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-08-23 13:59:01 -0400

    tweak nextcloud

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -5,63 +5,45 @@
   lib,
   ...
 }: {
-  services = {
-    nextcloud = {
-      enable = true;
-      home = "/srv/nextcloud";
-
-      hostName = config.networking.hostName;
-
-      # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud28;
-
-      # Let NixOS install and configure the database automatically.
-      database.createLocally = true;
-
-      # Let NixOS install and configure Redis caching automatically.
-      configureRedis = true;
-
-      # Increase the maximum file upload size to avoid problems uploading videos.
-      maxUploadSize = "1024G";
-      https = true;
-
-      autoUpdateApps.enable = true;
-      extraAppsEnable = true;
-      extraApps = with config.services.nextcloud.package.packages.apps; {
-        # List of apps we want to install and are already packaged in
-        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
-        inherit calendar contacts;
-      };
-      settings = {
-        overwriteprotocol = "https";
-      };
-      config = {
-        trustedProxies = ["127.0.0.1"];
-        dbtype = "pgsql";
-        dbuser = "nextcloud";
-        dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-        dbname = "nextcloud";
-        adminuser = "admin";
-        adminpassFile = config.sops.secrets.nextcloud_admin_pass.path;
-      };
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud29; # Required to specify
+    configureRedis = true;
+    datadir = "/srv/nextcloud";
+    database.createLocally = true;
+    https = true;
+    hostName = "localhost";
+    maxUploadSize = "128G";
+    config = {
+      adminpassFile = config.sops.secrets.nextcloud_admin_pass.path;
+      dbtype = "pgsql";
+    };
+    settings = {
+      default_phone_region = "US";
+      # Allow access when hitting either of these hosts or IPs
+      trusted_domains = [config.hostnames.content];
+      trusted_proxies = ["127.0.0.1"];
+      log_type = "file";
+      loglevel = 1; # Include all actions in the log
+    };
+    autoUpdateApps.enable = true;
+    extraAppsEnable = true;
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      # List of apps we want to install and are already packaged in
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+      inherit calendar contacts;
+    };
+    phpOptions = {
+      "opcache.interned_strings_buffer" = "16";
+      "output_buffering" = "0";
+    };
+    configureRedis = true;
+    settings = {
+      overwriteprotocol = "https";
     };
   };
 
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = ["nextcloud"];
-    ensureUsers = [
-      #     {
-      #       name = "nextcloud";
-      #       ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-      #     }
-    ];
-  };
-
-  systemd.services."nextcloud-setup" = {
-    requires = ["postgresql.service"];
-    after = ["postgresql.service"];
-  };
+  services.nginx.enable = false;
 
   services.phpfpm.pools.nextcloud.settings = {
     "listen.owner" = config.services.caddy.user;
@@ -69,10 +51,8 @@
   };
   users.users.caddy.extraGroups = ["nextcloud"];
 
-  services.nginx.enable = false;
-
   systemd.tmpfiles.rules = [
-    "d ${config.services.nextcloud.home} 0770 nextcloud nextcloud - -"
+    "d ${config.services.nextcloud.datadir} 0770 nextcloud nextcloud - -"
   ];
 
   sops.secrets = {
@@ -82,22 +62,143 @@
     };
   };
 
-  services.caddy = {
-    virtualHosts = {
-      "nextcloud.skyseekerhomelab.duckdns.org" = {
-        #       useACMEHost = "nextcloud.skyseekerhomelab.duckdns.org";
-        extraConfig = ''
-          redir /.well-known/carddav /remote.php/dav 301
-          redir /.well-known/caldav /remote.php/dav 301
-          redir /.well-known/webfinger /index.php/.well-known/webfinger 301
-          redir /.well-known/nodeinfo /index.php/.well-known/nodeinfo 301
-
-          encode gzip
-          reverse_proxy localhost:8009
-        '';
-      };
-    };
+  options.caddy.routes = lib.mkOption {
+    type = lib.types.listOf lib.types.attrs;
+    description = "Caddy JSON routes for http servers";
+    default = [];
   };
+
+  caddy.routes = [
+    {
+      match = [{host = ["nextcloud.skyseekerhomelab.duckdns.org"];}];
+      handle = [
+        {
+          handler = "subroute";
+          routes = [
+            # Sets variables and headers
+            {
+              handle = [
+                {
+                  handler = "vars";
+                  # Grab the webroot out of the written config
+                  # The webroot is a symlinked combined Nextcloud directory
+                  root = config.services.nginx.virtualHosts.${config.services.nextcloud.hostName}.root;
+                }
+                {
+                  handler = "headers";
+                  response.set.Strict-Transport-Security = ["max-age=31536000;"];
+                }
+              ];
+            }
+            # Reroute carddav and caldav traffic
+            {
+              match = [
+                {
+                  path = [
+                    "/.well-known/carddav"
+                    "/.well-known/caldav"
+                  ];
+                }
+              ];
+              handle = [
+                {
+                  handler = "static_response";
+                  headers = {
+                    Location = ["/remote.php/dav"];
+                  };
+                  status_code = 301;
+                }
+              ];
+            }
+            # Block traffic to sensitive files
+            {
+              match = [
+                {
+                  path = [
+                    "/.htaccess"
+                    "/data/*"
+                    "/config/*"
+                    "/db_structure"
+                    "/.xml"
+                    "/README"
+                    "/3rdparty/*"
+                    "/lib/*"
+                    "/templates/*"
+                    "/occ"
+                    "/console.php"
+                  ];
+                }
+              ];
+              handle = [
+                {
+                  handler = "static_response";
+                  status_code = 404;
+                }
+              ];
+            }
+            # Redirect index.php to the homepage
+            {
+              match = [
+                {
+                  file = {
+                    try_files = ["{http.request.uri.path}/index.php"];
+                  };
+                  not = [{path = ["*/"];}];
+                }
+              ];
+              handle = [
+                {
+                  handler = "static_response";
+                  headers = {
+                    Location = ["{http.request.orig_uri.path}/"];
+                  };
+                  status_code = 308;
+                }
+              ];
+            }
+            # Rewrite paths to be relative
+            {
+              match = [
+                {
+                  file = {
+                    split_path = [".php"];
+                    try_files = [
+                      "{http.request.uri.path}"
+                      "{http.request.uri.path}/index.php"
+                      "index.php"
+                    ];
+                  };
+                }
+              ];
+              handle = [
+                {
+                  handler = "rewrite";
+                  uri = "{http.matchers.file.relative}";
+                }
+              ];
+            }
+            # Send all PHP traffic to Nextcloud PHP service
+            {
+              match = [{path = ["*.php"];}];
+              handle = [
+                {
+                  handler = "reverse_proxy";
+                  transport = {
+                    protocol = "fastcgi";
+                    split_path = [".php"];
+                  };
+                  upstreams = [{dial = "unix//run/phpfpm/nextcloud.sock";}];
+                }
+              ];
+            }
+            # Finally, send the rest to the file server
+            {handle = [{handler = "file_server";}];}
+          ];
+        }
+      ];
+      terminal = true;
+    }
+  ];
 
   networking.firewall.allowedTCPPorts = [
     443
@@ -109,7 +210,7 @@
   # persistence
   environment.persistence."/nix/state".directories = [
     {
-      directory = config.services.nextcloud.home;
+      directory = config.services.nextcloud.datadir;
       #       inherit user group;
     }
   ];
