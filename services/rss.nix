d9a47c3831cbb40aee9fa2a89c8d19be644b20c0
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-05 19:18:29 -0400

    add rss-bridge

--- services/rss.nix
+++ services/rss.nix
@@ -5,7 +5,12 @@
   inputs,
   lib,
   ...
-}: {
+} @ args: {
+  disabledModules = ["nixos/modules/services/web-apps/freshrss.nix"];
+  imports = [
+    "${args.inputs.nixpkgs-unstable}/path/to/module.nix"
+  ];
+
   # fseshrss
   services.freshrss = {
     enable = true;
