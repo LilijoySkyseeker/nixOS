e8fbbb2f0f3705aa6c5855625df89b7d8c9e2b16
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-05 19:00:11 -0400

    add rss-bridge

--- services/rss.nix
+++ services/rss.nix
@@ -12,6 +12,9 @@
     dataDir = "/srv/freshrss";
     defaultUser = "admin";
     passwordFile = config.sops.secrets.freshrss_admin_pass.path;
+    extensions = with pkgs.freshrss-extensions; [
+      reddit-image
+    ];
   };
 
   # rss-bridge
