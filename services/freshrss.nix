fc52debdfb239a368f6cb97128c6d31de1cd7c89
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-05 14:08:53 -0400

    tweak jellyfin

--- services/freshrss.nix
+++ services/freshrss.nix
@@ -8,7 +8,7 @@
   # fseshrss
   services.freshrss = {
     enable = true;
-    baseUrl = "freshrss.skyseekerlabs.duckdns.org";
+    #   baseUrl = "freshrss.skyseekerlabs.duckdns.org";
     dataDir = "/srv/freshrss";
     defaultUser = "admin";
     passwordFile = config.sops.secrets.freshrss_admin_pass.path;
