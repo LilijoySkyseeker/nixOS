15bf5629bce9141cf04b327d169e1f6c40e8e5fa
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-19 10:12:32 -0400

    tweak nextcloud

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -14,7 +14,7 @@
     };
     settings = {
       default_phone_region = "US";
-      trusted_domains = ["skyseekerlabs.duckdns.org"];
+      trusted_domains = ["localhost"];
       log_type = "file";
       loglevel = 1; # Include all actions in the log
     };
