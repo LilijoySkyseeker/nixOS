66435ce3ed33f6f3ce61ca50c50dc81e82df2a87
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-29 12:40:32 -0400

    remove auto-cpufreq

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -20,21 +20,6 @@
       # UNSTABLE installed packages
     ]);
 
-  # power managment
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-    };
-  };
-
   # update microcode
   hardware.cpu.intel.updateMicrocode = true;
 
