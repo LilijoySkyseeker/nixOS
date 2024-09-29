057aab73e91079db947d096788b6ddbe6b245017
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-09-29 10:42:48 -0400

    tweak nvidia settings

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -202,10 +202,10 @@
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
+    powerManagement.enable = false;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -214,7 +214,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu.
     # accessible via `nvidia-settings`.
