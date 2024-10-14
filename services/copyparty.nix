67dba6b99de7c36c9213e1bee8d1f2af892c446c
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-10-14 16:06:04 -0400

    tweak copyparty

--- services/copyparty.nix
+++ services/copyparty.nix
@@ -16,7 +16,6 @@
       no-robots = true;
       e2dsa = true;
       e2ts = true;
-      shr = true;
     };
     accounts = {
       lilijoy.passwordFile = config.sops.secrets.copyparty_lilijoy.path;
