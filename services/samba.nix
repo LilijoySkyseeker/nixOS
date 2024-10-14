d227182d34488faa3ffd68019ef9f653cfe587cf
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-10-14 15:14:40 -0400

    tweak samba

--- services/samba.nix
+++ services/samba.nix
@@ -1,25 +1,56 @@
-{inputs, ...}: {
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
   disabledModules = ["services/network-filesystems/samba.nix"];
   imports = [
     "${inputs.nixpkgs-unstable}/nixos/modules/services/network-filesystems/samba.nix"
   ];
+  users = {
+    groups.samba-guest = {};
+    users.samba-guest = {
+      isSystemUser = true;
+      description = "Residence of our Samba guest users";
+      group = "multimedia";
+      home = "/var/empty";
+      createHome = false;
+      shell = pkgs.shadow;
+    };
+  };
 
   services.samba = {
     enable = true;
     openFirewall = true;
-    settings = {
-      global = {
-        "invalid users" = [
-          "root"
-        ];
-        "passwd program" = "/run/wrappers/bin/passwd %u";
-        security = "user";
-      };
-      public = {
-        browseable = "yes";
-        comment = "Public samba share.";
-        "guest ok" = "yes";
+    extraConfig = ''
+      server string = ${config.networking.hostName}
+      netbios name = ${config.networking.hostName}
+      workgroup = WORKGROUP
+      security = user
+
+      create mask = 0664
+      force create mode = 0664
+      directory mask = 0775
+      force directory mode = 0775
+      follow symlinks = yes
+
+      # :NOTE| localhost is the ipv6 localhost ::1
+      #hosts allow = 192.168.0.0/16 localhost
+      #hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    # :NOTE| set sudo smbpasswd -a samba-guest -n
+    shares = {
+      Storage = {
         path = "/storage";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "force user" = "${config.user.name}";
+        "force group" = "multimedia";
+        "write list" = "${config.user.name}";
       };
     };
   };
