0dff42fc7dd351f3dc9fec7e1f8d907e1bf81a04
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2024-10-21 09:31:49 -0400

    fix mystype

--- services/factorio.nix
+++ services/factorio.nix
@@ -15,7 +15,6 @@
   ];
 
   # factorio server
-  virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers.factorio-main = {
     autoStart = true;
     image = "factoriotools/factorio";
