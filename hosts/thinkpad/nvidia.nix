d1499c9d16ac4dbd9e3a454d77394e4fc3884b51
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2025-05-07 11:05:17 -0400

    change gpu mode to reverse sync

--- hosts/thinkpad/nvidia.nix
+++ hosts/thinkpad/nvidia.nix
@@ -11,40 +11,19 @@
     services.xserver.videoDrivers = [ "nvidia" ]; # Load nvidia driver for Xorg and Wayland
     hardware.nvidia = {
       modesetting.enable = true;
-
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       powerManagement.enable = false;
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
       powerManagement.finegrained = true;
-
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of
-      # supported GPUs is at:
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-      # Only available from driver 515.43.04+
-      # Currently alpha-quality/buggy, so false is currently the recommended setting.
       open = true;
-
-      # Enable the Nvidia settings menu.
-      # accessible via `nvidia-settings`.
       nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.stable; # info for beta/specific drivers https://github.com/NixOS/nixpkgs/pull/322963/commits/10ed11d6856a7b67b9b2cef5e52af5c7de34b93f and reddit post https://www.reddit.com/r/NixOS/comments/1dqipyx/updating_nvidia_driver_from_5554202_to_the_latest/
     };
-
     hardware.nvidia.prime = {
       # Bus ID of the Intel GPU.
       intelBusId = lib.mkDefault "PCI:0:2:0";
       # Bus ID of the NVIDIA GPU.
       nvidiaBusId = lib.mkDefault "PCI:1:0:0";
-
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
+      # set primary render to NVIDIA gpu, but use intel gpu display outputs
+      reverseSync.enable = true;
     };
     # =====================================================================================
   };
