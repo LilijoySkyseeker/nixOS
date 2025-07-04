e36f838c48239b1fd2c97e780c34a9889363b725
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2025-07-04 13:07:02 -0700

    tweak locale

--- profiles/default.nix
+++ profiles/default.nix
@@ -135,12 +135,11 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "es_US.UTF-8";
   i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "es_US.UTF-8/UTF-8"
+    "all"
   ];
-  i18n.extraLocaleSettings = {
-    LC_ALL = "en_US.UTF-8";
-  };
+  # i18n.extraLocaleSettings = {
+  #   LC_ALL = "en_US.UTF-8";
+  # };
 
   # x86_64
   nixpkgs.hostPlatform = "x86_64-linux";
