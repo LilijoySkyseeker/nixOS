67f61bd1acb767f4b4def5703c1247cc8415e719
LilijoySkyseeker <lilijoyskyseeker@gmail.com>
2025-07-04 12:55:25 -0700

    add spanish locale

--- profiles/default.nix
+++ profiles/default.nix
@@ -136,7 +136,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
-    "es_MX.UTF-8/UTF-8"
+    "es_US.UTF-8/UTF-8"
   ];
   i18n.extraLocaleSettings = {
     LC_ALL = "en_US.UTF-8";
